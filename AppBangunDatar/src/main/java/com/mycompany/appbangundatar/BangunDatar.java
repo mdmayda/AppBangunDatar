a0c048e46cd242d7a7e44b532baf759a5dd5971d
mdmayda <mdmayda123@gmail.com>
2022-12-20 10:46:29 +0700

    initiate commit project

---
+++ AppBangunDatar/src/main/java/com/mycompany/appbangundatar/BangunDatar.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.appbangundatar;
+
+/**
+ *
+ * @author User
+ */
+abstract class BangunDatar {
+    private String namaBangunDatar;
+
+    public BangunDatar() {
+    }
+
+    public BangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    abstract double luas();
+    abstract double keliling();
+    
+}
